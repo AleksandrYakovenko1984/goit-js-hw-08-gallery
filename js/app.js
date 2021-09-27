820eaa0fd1bfd9a22329237902a3d23f4ffa4f20
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2021-09-27 23:28:36 +0300

    modal

--- js/app.js
+++ js/app.js
@@ -35,6 +35,51 @@ function createItems(array) {
 
 }
 const markup = createItems(prod)
-console.log(markup);
+// console.log(markup);
 // встраивание разметки в список
 list.insertAdjacentHTML('afterbegin', markup)
+
+// открытие модалки
+function showElement(element) {
+  element.classList.add('is-open')
+  
+}
+function hideElement(element) {
+    element.classList.remove('is-open')
+  }
+list.addEventListener('click', (e) => {
+  const condition = e.target.nodeName === 'IMG'
+  console.log(condition);
+  e.preventDefault()
+  if (condition) {
+    showElement(modalDiv)
+
+      const modalImage = document.createElement('img')
+    const link = prod.find((object) => object.id === e.target.id).image
+    modalImage.setAttribute('src', link)
+    console.log(modalImage)
+
+    modalContent.append(modalImage)
+
+  }
+})
+// закрытие модального окна
+// по бекдропу
+// modalDiv.addEventListener('click', () => {
+//   console.log(e.target.classList.contains('lightbox__overlay'));
+// })
+// по кнопке х
+closeBtn.addEventListener('click', () => {
+  hideElement(modalDiv)
+  // modalDiv.classList.remove('is-open')
+})
+// по кнопке эскейп, влво, вправо
+window.addEventListener('keydown', (e) => {
+  const condition = e.code === 'Escape' || e.code === 'ArrowLeft' || e.code === 'ArrowRight'
+  if (condition) {
+    // modalDiv.classList.remove('is-open')
+    hideElement(modalDiv)
+  }
+  
+})
+// 
